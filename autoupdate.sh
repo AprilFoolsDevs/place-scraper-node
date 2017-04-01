59ee1fb441f5019317fa1c60e8b4a83b7d331c4e
7coil <leondrolio@gmail.com>
2017-04-01 19:02:21 +0100

    Fix password

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,7 +1,7 @@
 while true
 do
     rm /tmp/place.txt
-    mysqldump -u reddit --p redditsips reddit place --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0a --tab /tmp
+    mysqldump -u reddit -predditsips reddit place --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0a --tab /tmp
     cp /tmp/place.txt /var/www/html/place/export.csv
     sleep 300
 done
