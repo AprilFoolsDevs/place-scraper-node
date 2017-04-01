55c315792a2444f69cfbd522f3ff6b94f5e68f15
7coil <leondrolio@gmail.com>
2017-04-01 18:58:57 +0100

    I don't know how to bash

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,7 @@
-while (true) do {
+while true
+do
     rm /tmp/place.txt
     mysqldump -p reddit place --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0a --tab /tmp
     cp /tmp/place.txt /var/www/html/place/export.csv
     sleep 300
-}
+done
