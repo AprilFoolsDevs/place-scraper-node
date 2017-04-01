d3f72659476fe24d5f1f90bbbe00bbfe56b6212b
Hacksore <hacksore@mcskinsearch.com>
2017-04-01 15:53:51 -0500

    dev branch with some new stuffz

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 var WebSocket = require('ws');
 var cheerio = require('cheerio');
-var request = require('request');
+var rp = require('request-promise');
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: '192.168.0.100',
-	user: 'reddit',
-	password: 'redditsips',
-	database: 'reddit'
-});
+var Promise = require("bluebird");
+var fs = require("fs");
+
+var conf = JSON.parse(fs.readFileSync("config.json"));
+
+var connection = mysql.createConnection(conf);
 connection.connect();
 
-request('https://reddit.com/place', function (error, response, body) {
-	var $ = cheerio.load(body);
-	//Get the config in the HTML
-	//Cut 8 from front, 1 from end
-	//Parse this as JSON
-	//Find the websocket URL and set it as wsurl
-	wsurl = JSON.parse($('#config').text().slice(0,-1).substring(8)).place_websocket_url;
-	console.log(wsurl);
-	var ws = new WebSocket(wsurl);
-
-	ws.on('message', function(input) {
-		var d = new Date();
-		var message = JSON.parse(input).payload;
-		console.dir(message);
-		if(message.x && message.y && message.author && message.color) {
-			var query = "INSERT INTO place (x, y, username, colour, time) VALUES (" + message.x + "," + message.y + ",'" + message.author + "'," + message.color + ",'" + d.getTime() + "');";
-			connection.query(query);
-		}
+function getSocketURL(){
+	return new Promise(function(resolve, reject){
+		rp('https://reddit.com/place')
+		.then(function(body){
+			var $ = cheerio.load(body);
+			wsurl = JSON.parse($('#config').text().slice(0,-1).substring(8)).place_websocket_url;
+			resolve(wsurl)
+		});
 	});
-});
+}
+
+function isValidMessage(message){
+	return (message.x || message.x === 0) && (message.y || message.y === 0) && message.author && (message.color || message.color === 0));
+}
+
+function createSocketHandler(){
+
+	getSocketURL()
+	.then( (wsurl) => {
+		var ws = new WebSocket(wsurl);
+		console.log("Connecting to " + wsurl);
+		ws.on('message', function(input) {
+			var d = new Date();
+			var message = JSON.parse(input).payload;
+
+			if(isValidMessage(message)) {
+				var query = "INSERT INTO place (x, y, username, color, time) VALUES (" + message.x + "," + message.y + ",'" + message.author + "'," + message.color + ",'" + d.getTime() + "');";
+				connection.query(query);
+			}
+
+		});
+
+		ws.on('close', function close() {
+			console.log("We lost connection since the server was destroyed, let's reconnect :)");
+			createSocketHandler();
+		});
+
+	});
+}
+
+createSocketHandler();
