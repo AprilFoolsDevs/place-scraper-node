4808edbbb26de5cbf1ae97b4e4344bf4e1dff29d
7coil <leondrolio@gmail.com>
2017-04-01 18:53:08 +0100

    Create autoupdate.sh

---
+++ autoupdate.sh
@@ -0,0 +1,6 @@
+while (true) {
+    rm /tmp/place.txt
+    mysqldump -p reddit place --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0a --tab /tmp
+    cp /tmp/place.txt /var/www/html/place/export.csv
+    sleep 300
+}
