5a734f65787c511bbe932394d8cce3db9c06febd
7coil <leondrolio@gmail.com>
2017-04-01 18:57:18 +0100

    Forgot DO

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,4 +1,4 @@
-while (true) {
+while (true) do {
     rm /tmp/place.txt
     mysqldump -p reddit place --fields-terminated-by=, --fields-enclosed-by='"' --lines-terminated-by=0x0a --tab /tmp
     cp /tmp/place.txt /var/www/html/place/export.csv
